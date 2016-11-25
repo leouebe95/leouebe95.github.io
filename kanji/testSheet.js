9d3d6a53b3aef9f6e30042e7206c29aa67a548d9
leouebe <leouebe95@gmail.com>
2016-11-25 20:24:32 +0000

    UI

--- kanji/testSheet.js
+++ kanji/testSheet.js
@@ -1,6 +1,10 @@
 (function() {
     "use strict";
-    function setData(inputString) {
+    function setData(data) {
+        var inputString = data.data.value;
+        var shuffle = data.shuffle.checked;
+        var maxlines = data.maxlines.value;
+
         // Normalize the Japanese characters used for syntax
         var remap = {
             '（': '(',
@@ -17,61 +21,89 @@
 
         var lines = inputString.trim().split('\n');
 
-        var top = document.getElementById("result");
+        var top = document.getElementById('result');
 
         // Empty everything first
         top.innerHTML = '';
 
-        var area = document.createElement("div"); // Create a <div> per area
-        area.classList.add("flushright");
+        var area = document.createElement('div'); // Create a <div> per area
+        area.classList.add('flushright');
         top.appendChild(area);
+        var nbLines = 0;
+        var indirect = [];
+        for (var i = 0; i<lines.length; i++) {
+            indirect.push(i);
+        }
+
+        if (shuffle) {
+            for (var ii=lines.length-1 ; ii>1 ; ii--) {
+                var jj = Math.floor(Math.random()*ii);
+                var tmp = indirect[ii];
+                indirect[ii] = indirect[jj];
+                indirect[jj] = tmp;
+            }
+        }
 
         for (var i=0 ; i<lines.length ; i++) {
-            var line = lines[i].trim();
+            var line = lines[indirect[i]].trim();
             if (line === '') {
-                // Create a new section
-                var sep = document.createElement("hr");
-                top.appendChild(sep);
-                area = document.createElement("div");
-                area.classList.add("flushright");
-                top.appendChild(area);
-            } else {
-                if (area.hasChildNodes()) {
-                    var space = document.createElement("div");
-                    space.classList.add("blank");
-                    area.insertBefore(space, area.firstChild);
-                }
-
-                // Real practice line. X(xxx) is a kanji followed by
-                // phonetics
-                line = line.replace(/(.)\(([^()]+)\)/g,
-                                    '<ruby><rb>$1</rb><rt>$2</rt></ruby>')
-
-                var elem = document.createElement("span");
-                elem.classList.add("practice");
-                elem.innerHTML = line;
-                area.insertBefore(elem, area.firstChild);
+                continue;
             }
+
+            if (nbLines >= maxlines) {
+                nbLines = 0;
+                // Create a new section
+                var sep = document.createElement('hr');
+                top.appendChild(sep);
+                area = document.createElement('div');
+                area.classList.add('flushright');
+                top.appendChild(area);
+            }
+            nbLines++;
+
+            if (area.hasChildNodes()) {
+                var space = document.createElement('div');
+                space.classList.add('blank');
+                area.insertBefore(space, area.firstChild);
+            }
+
+            // Real practice line. X(xxx) is a kanji followed by
+            // phonetics
+            line = line.replace(/(.)\(([^()]+)\)/g,
+                                '<ruby><rb>$1</rb><rt>$2</rt></ruby>')
+
+            var elem = document.createElement('span');
+            elem.classList.add('practice');
+            elem.innerHTML = line;
+            area.insertBefore(elem, area.firstChild);
         }
     }
 
+    var inputs = ['data', 'shuffle', 'maxlines'];
+
     function refreshPage() {
-        var data = document.getElementById("data");
-        setData(data.value);
+        var data = {};
+        for (var i=0 ; i<inputs.length ; i++) {
+            data[inputs[i]] = document.getElementById(inputs[i]);
+        }
+        setData(data);
     }
 
     function refreshStyle() {
-        var mode = document.getElementById("mode");
-        var data = document.getElementById("result");
+        var mode = document.getElementById('mode');
+        var data = document.getElementById('result');
         data.className = mode.value;
     }
 
     function setup() {
-        var data = document.getElementById("data");
-        data.addEventListener("change", refreshPage);
+        for (var i=0 ; i<inputs.length ; i++) {
+            var data = document.getElementById(inputs[i]);
+            data.addEventListener('change', refreshPage);
+        }
 
-        data = document.getElementById("mode");
-        data.addEventListener("change", refreshStyle);
+        data = document.getElementById('mode');
+        data.addEventListener('change', refreshStyle);
+
         refreshPage();
         refreshStyle();
     }
