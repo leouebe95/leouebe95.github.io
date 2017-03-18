549233873f6ab1b86ec818737fc925bcd7337d3a
leouebe <leouebe95@gmail.com>
2017-03-18 11:57:43 +0100

    ESlint

--- kanji/testSheet.js
+++ kanji/testSheet.js
@@ -1,5 +1,11 @@
 (function() {
-    "use strict";
+    'use strict';
+    
+    /**
+       Recompute the final sheet based on user parameters.
+
+       @param {Object} data collected data from the UI
+    */
     function setData(data) {
         var inputString = data.data.value;
         var shuffle = data.shuffle.checked;
@@ -7,31 +13,31 @@
 
         // Normalize the Japanese characters used for syntax
         var remap = {
-            "（": "(",
-            "〔": "(",
-            "【": "(",
+            '（': '(',
+            '〔': '(',
+            '【': '(',
 
-            "）": ")",
-            "〕": ")",
-            "】": ")"
+            '）': ')',
+            '〕': ')',
+            '】': ')'
         };
         for(var key in remap) {
-            inputString = inputString.replace(new RegExp(key, "g"), remap[key]);
+            inputString = inputString.replace(new RegExp(key, 'g'), remap[key]);
         }
 
-        var lines = inputString.trim().split("\n");
+        var lines = inputString.trim().split('\n');
 
-        var top = document.getElementById("result");
+        var top = document.getElementById('result');
 
         // Empty everything first
-        top.innerHTML = "";
+        top.innerHTML = '';
 
-        var area = document.createElement("div"); // Create a <div> per area
-        area.classList.add("flushright");
+        var area = document.createElement('div'); // Create a <div> per area
+        area.classList.add('flushright');
         top.appendChild(area);
         var nbLines = 0;
         var indirect = [];
-        for (var i = 0; i<lines.length; i++) {
+        for (let i = 0; i<lines.length; i++) {
             indirect.push(i);
         }
 
@@ -44,56 +50,46 @@
             }
         }
 
-        for (i=0 ; i<lines.length ; i++) {
+        for (let i=0 ; i<lines.length ; i++) {
             var line = lines[indirect[i]].trim();
-            if (line === "") {
+            if (line === '') {
                 continue;
             }
 
             if (nbLines >= maxlines) {
                 nbLines = 0;
                 // Create a new section
-                var sep = document.createElement("hr");
+                var sep = document.createElement('hr');
                 top.appendChild(sep);
-                area = document.createElement("div");
-                area.classList.add("flushright");
+                area = document.createElement('div');
+                area.classList.add('flushright');
                 top.appendChild(area);
             }
             nbLines++;
 
             if (area.hasChildNodes()) {
-                var space = document.createElement("div");
-                space.classList.add("blank");
+                var space = document.createElement('div');
+                space.classList.add('blank');
                 area.insertBefore(space, area.firstChild);
             }
 
             // Real practice line. X(xxx) is a kanji followed by
             // phonetics
             line = line.replace(/(.)\(([^()]+)\)/g,
-                                "<ruby><rb>$1</rb><rt>$2</rt></ruby>");
+                                '<ruby><rb>$1</rb><rt>$2</rt></ruby>');
 
-            var elem = document.createElement("span");
-            elem.classList.add("practice");
+            var elem = document.createElement('span');
+            elem.classList.add('practice');
             elem.innerHTML = line;
             area.insertBefore(elem, area.firstChild);
         }
     }
 
-    var inputs = ["data", "shuffle", "maxlines"];
+    var inputs = ['data', 'shuffle', 'maxlines'];
 
-    function updatePredefinedSet() {
-        var predef = document.getElementById("predef");
-        var set = predef.value;
-        var inString = window.inputData[set]
-        if (inString) {
-            inString = inString.replace(/ /g, "");
-            inString = inString.replace(/^\n/g, "");
-            var data = document.getElementById("data");
-            data.value = inString;
-            refreshPage();
-        }
-    }
-    
+    /**
+       Refresh the page after someinput changes.
+    */
     function refreshPage() {
         var data = {};
         for (var i=0 ; i<inputs.length ; i++) {
@@ -102,28 +98,52 @@
         setData(data);
     }
 
+    /**
+       Switch to one predefined set and refresh the page.
+    */
+    function updatePredefinedSet() {
+        var predef = document.getElementById('predef');
+        var set = predef.value;
+        var inString = window.inputData[set];
+        if (inString) {
+            inString = inString.replace(/ /g, '');
+            inString = inString.replace(/^\n/g, '');
+            var data = document.getElementById('data');
+            data.value = inString;
+            refreshPage();
+        }
+    }
+
+    /**
+       Change the practice sheep display style and refresh the page.
+    */
     function refreshStyle() {
-        var mode = document.getElementById("mode");
-        var data = document.getElementById("result");
+        var mode = document.getElementById('mode');
+        var data = document.getElementById('result');
         data.className = mode.value;
     }
 
+    /**
+       Setup the page: configure UI callbacks and expand the web
+       components. 
+    */
     function setup() {
+        var data;
         for (var i=0 ; i<inputs.length ; i++) {
-            var data = document.getElementById(inputs[i]);
-            data.addEventListener("change", refreshPage);
+            data = document.getElementById(inputs[i]);
+            data.addEventListener('change', refreshPage);
         }
 
-        data = document.getElementById("mode");
-        data.addEventListener("change", refreshStyle);
+        data = document.getElementById('mode');
+        data.addEventListener('change', refreshStyle);
 
-        data = document.getElementById("predef");
-        data.addEventListener("change", updatePredefinedSet);
-        
+        data = document.getElementById('predef');
+        data.addEventListener('change', updatePredefinedSet);
+
         updatePredefinedSet();
         refreshPage();
         refreshStyle();
     }
 
-    document.addEventListener("DOMContentLoaded", setup);
+    document.addEventListener('DOMContentLoaded', setup);
 })();
