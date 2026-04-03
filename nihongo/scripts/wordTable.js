759b81f8fdd66324f234510374e0675e279a92bf
leouebe95 <leouebe95@gmail.com>
2026-04-03 15:23:51 +0200

    Improved verb conjugation link

--- nihongo/scripts/wordTable.js
+++ nihongo/scripts/wordTable.js
@@ -5,9 +5,16 @@
    Formats a wordlist into a table suitable for printing
 */
 class WordTable { // eslint-disable-line no-unused-vars
+    static verbURL = 'https://www.japaneseverbconjugator.com/VerbDetails.asp?txtVerb=${shortRef}&Go=Conjugate';
+    static prefixes =
+        {
+            'verb (1 dan)': { icon: '&#x2460;', url: WordTable.verbURL }, // ①
+            'verb (5 dan)': { icon: '&#x2464;', url: WordTable.verbURL }, // ⑤
+            'verb (irreg)': { icon: '&#x24CB;', url: WordTable.verbURL }, // Ⓥ
+        };
 
     // ------------------------------------------------------------------------
-    constructor(wordLen, nbCol=2) {
+    constructor(wordLen, nbCol = 2) {
         this._wordLen = wordLen;
         this._nbCol = nbCol;
 
@@ -18,17 +25,17 @@ class WordTable { // eslint-disable-line no-unused-vars
     initTable() {
 
         // Compute how many lines to place in each column
-        this._linePerCol = Math.ceil(this._wordLen/this._nbCol);
+        this._linePerCol = Math.ceil(this._wordLen / this._nbCol);
         this._colIndx = 0;
         this._rowIndx = 0;
-        let width = Math.floor(100/this._nbCol);
+        let width = Math.floor(100 / this._nbCol);
 
         // Create all columns
         this._columns = [];
-        for (let i=0 ; i<this._nbCol ; i++) {
+        for (let i = 0; i < this._nbCol; i++) {
 
             var col = document.createElement("table");
-            col.classList.add(i%2 ? 'odd' : 'even');
+            col.classList.add(i % 2 ? 'odd' : 'even');
             col.style.borderWidth = `${width}px`;
             this._columns.push(col);
         }
@@ -55,11 +62,10 @@ class WordTable { // eslint-disable-line no-unused-vars
        column when it is full.
     */
     addLine(item) {
-        console.log(item)
         var newLine = document.createElement("tr");
         var kanji = item['Kanji'];
-        var kana  = item['Kana'];
-        var ref   = kanji;
+        var kana = item['Kana'];
+        var ref = kanji;
         var link = null;
 
         if (kanji == kana) {
@@ -71,12 +77,15 @@ class WordTable { // eslint-disable-line no-unused-vars
             kanji = `<a target="_new" href="${link}">${kanji}</a>`;
         }
 
-        if (item['Category'].startsWith('verb')) {
+        if (item['Category'] in WordTable.prefixes) {
+            var prefix = WordTable.prefixes[item['Category']];
             // In the Web page, display a link to the verb conjugation page.
             // mark it as no-print.
             var shortRef = ref.split('・')[0]
-            link = encodeURI(`https://www.japaneseverbconjugator.com/VerbDetails.asp?txtVerb=${shortRef}&Go=Conjugate`);
-            kanji = `<a class="noprint" target="_new" href="${link}">&#x24CB;</a> ` + kanji;
+            link = encodeURI(prefix.url.replace('${shortRef}', shortRef));
+            var icon = prefix.icon;
+
+            kanji = `<a class="noprint" target="_new" href="${link}">${icon}</a> ` + kanji;
         }
 
         newLine.innerHTML = `
@@ -87,10 +96,10 @@ class WordTable { // eslint-disable-line no-unused-vars
 `;
 
         this._columns[this._colIndx].appendChild(newLine);
-        this._rowIndx ++;
+        this._rowIndx++;
         if (this._rowIndx >= this._linePerCol) {
             this._rowIndx = 0;
-            this._colIndx ++;
+            this._colIndx++;
         }
     }
 }
