e6813b29bf124d3bcd0e8afd617aae08edc9fba1
leouebe95 <leouebe95@gmail.com>
2026-04-08 20:08:27 +0200

    direction step 1

--- nihongo/CountersPoster/src/counters.js
+++ nihongo/CountersPoster/src/counters.js
@@ -4,21 +4,26 @@ function buildTable(root, mode) {
     root.innerHTML = '';
     var cards = new counterCard(mode);
 
-    var topics = [
+    var topicsV = [
         ["thing", "small", "people", "age"],
         ["floor", "order", "frequency", "animal"],
         ["flat", "long", "book", "drink"],
         ["vehicle", "house", "sock", "clothe"]
     ];
 
-    var content = cards.makeCards(db, topics);
+    var topicsH = [
+        ["thing", "small", "people", "age", "floor", "order", "frequency", "animal"],
+        ["flat", "long", "book", "drink", "vehicle", "house", "sock", "clothe"]
+    ];
+
+    var content = cards.makeCards(db, topicsV);
     root.appendChild(content);
 }
 
 function handleModeChange(event) {
     const selectedModeStr = event.target.value;
-    localStorage.setItem('counterPosterMode', selectedModeStr);
-    
+    localStorage.setItem('CountersMode', selectedModeStr);
+
     var main = document.getElementById("mainId");
     buildTable(main, counterCard.MODES[selectedModeStr]);
 }
@@ -27,7 +32,7 @@ function bootStrap() {
     db = new counterDB(counterData);
 
     // Initialize UI from local storage
-    let savedModeStr = localStorage.getItem('counterPosterMode') || "KANA";
+    let savedModeStr = localStorage.getItem('CountersMode') || "KANA";
     if (!counterCard.MODES.hasOwnProperty(savedModeStr)) {
         savedModeStr = "KANA";
     }
